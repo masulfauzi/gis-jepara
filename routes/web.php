e4094d93e2911a3a7f215da2ccff2b485911ba05
masulfauzi <masul.fauzi@gmail.com>
2023-11-22 16:35:59 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('frontend.index');
+Route::view('/', 'landing')->name('frontend.index');
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
